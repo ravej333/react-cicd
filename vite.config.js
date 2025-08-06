2c8d16554cfc63c67f359740d597ca12956bb151
ravej333 <ravejbasha@gmail.com>
2025-08-06 07:52:12 +0530

    Version 2 Commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/react-cicd/', // Must match your GitHub repo name
+  base: '/react-cicd/', // 👈 MUST MATCH your repo name
   plugins: [react()],
 });
